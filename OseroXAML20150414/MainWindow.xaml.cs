0fce79228f59a1c1d53f7524c339609d1210f916
RauziDssh <bmn1070@mail4.doshisha.ac.jp>
2015-04-15 12:51:21 +0900

    はじめの盤面表示まで

---
+++ OseroXAML20150414/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OseroXAML20150414
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<stone> Field = new List<stone> { 
+                new stone{color = Brushes.White,Pos_column = 3,Pos_row = 3},
+                new stone{color = Brushes.White,Pos_column = 4,Pos_row = 4},
+                new stone{color = Brushes.Black,Pos_column = 3,Pos_row = 4},
+                new stone{color = Brushes.Black,Pos_column = 4,Pos_row = 3},
+            };
+            this.DataContext = new
+            {
+                Field = Field,
+                Turn = Brushes.Black,
+            };
+            
+        }
+
+        private void Grid_Field_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //getCellIndexfromPosition(e.GetPosition());
+        }
+
+        private int[] getCellIndexfromPosition(Point p)
+        {
+            int[] cell = new int[2];
+            return cell;
+        }
+
+    }
+
+    public class stone
+    {
+        public Brush color { get; set; }
+        public int Pos_column { get; set; }
+        public int Pos_row { get; set; }
+    } 
+}
